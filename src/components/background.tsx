d470b64709ff2a6849f84d4f4c5fc02463469a69
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-03-03 21:31:05 -0800

    change pictures and background add pictures and background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -14,6 +14,18 @@ import pic12 from "../assets/material/pic12.jpg";
 import pic13 from "../assets/material/pic13.jpg";
 import pic14 from "../assets/material/pic14.jpg";
 import pic15 from "../assets/material/pic15.jpg";
+import pic16 from "../assets/material/pic16.jpg";
+import pic17 from "../assets/material/pic17.jpg";
+import pic18 from "../assets/material/pic18.jpg";
+import pic19 from "../assets/material/pic19.jpg";
+import pic20 from "../assets/material/pic20.jpg";
+import pic21 from "../assets/material/pic21.jpg";
+import pic22 from "../assets/material/pic22.jpg";
+import pic23 from "../assets/material/pic23.jpg";
+import pic24 from "../assets/material/pic24.jpg";
+import pic25 from "../assets/material/pic25.jpg";
+import pic26 from "../assets/material/pic26.jpg";
+import pic27 from "../assets/material/pic27.jpg";
 import epic01 from "../assets/electrical/pic01.jpg";
 import epic02 from "../assets/electrical/pic02.jpg";
 import epic03 from "../assets/electrical/pic03.jpg";
@@ -29,7 +41,17 @@ import epic12 from "../assets/electrical/pic12.jpg";
 import epic13 from "../assets/electrical/pic13.jpg";
 import epic14 from "../assets/electrical/pic14.jpg";
 import epic15 from "../assets/electrical/pic15.jpg";
-
+import epic16 from "../assets/electrical/pic16.jpg";
+import epic17 from "../assets/electrical/pic17.jpg";
+import epic18 from "../assets/electrical/pic18.jpg";
+import epic19 from "../assets/electrical/pic19.jpg";
+import epic20 from "../assets/electrical/pic20.jpg";
+import epic21 from "../assets/electrical/pic21.jpg";
+import epic22 from "../assets/electrical/pic22.jpg";
+import epic23 from "../assets/electrical/pic23.jpg";
+import epic24 from "../assets/electrical/pic24.jpg";
+import epic25 from "../assets/electrical/pic25.jpg";
+import epic26 from "../assets/electrical/pic26.jpg";
 import "../styles/background.css"
 
 export const Background = () => {
@@ -49,6 +71,18 @@ export const Background = () => {
     pic13,
     pic14,
     pic15,
+    pic16,
+    pic17,
+    pic18,
+    pic19,
+    pic20,
+    pic21,
+    pic22,
+    pic23,
+    pic24,
+    pic25,
+    pic26,
+    pic27,
   ];
   const arrayElectrical = [
     epic01,
@@ -66,6 +100,17 @@ export const Background = () => {
     epic13,
     epic14,
     epic15,
+    epic16,
+    epic17,
+    epic18,
+    epic19,
+    epic20,
+    epic21,
+    epic22,
+    epic23,
+    epic24,
+    epic25,
+    epic26,
   ];
   const [row01, setrow01] = useState<string[]>([
     pic01,
